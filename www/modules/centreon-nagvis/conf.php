8f67b9410f8e78d2d219baa90a786dd0b6047a36
Quentin Delance <qdelance@merethis.com>
2015-02-19 17:30:31 +0100

    Ref #6145 Purge old unused *.tpl files Start merging code from contributor

--- www/modules/centreon-nagvis/conf.php
+++ www/modules/centreon-nagvis/conf.php
@@ -1,10 +1,10 @@
 <?php
 $module_conf['centreon-nagvis']['rname'] = 'Centreon NagVis';
 $module_conf['centreon-nagvis']['name'] = 'centreon-nagvis';
-$module_conf['centreon-nagvis']['mod_release'] = '1.0.3';
+$module_conf['centreon-nagvis']['mod_release'] = '1.1';
 $module_conf['centreon-nagvis']['infos'] = 'NagVis module for Centreon';
 $module_conf['centreon-nagvis']['is_removeable'] = '1';
-$module_conf['centreon-nagvis']['author'] = 'Merethis Team';
+$module_conf['centreon-nagvis']['author'] = 'Centreon';
 $module_conf['centreon-nagvis']['lang_files'] = '0';
 $module_conf['centreon-nagvis']['sql_files'] = '1';
 $module_conf['centreon-nagvis']['php_files'] = '0';
--- www/modules/centreon-nagvis/index.php
+++ www/modules/centreon-nagvis/index.php
@@ -5,7 +5,6 @@ if (!isset($centreon)) {
 }
 
 $path = dirname(__FILE__);
-$roUser = 'centreon_nagvis';
 
 /* Get nagvis path */
 $query = 'SELECT `key`, `value` FROM `options` WHERE `key` IN ("centreon_nagvis_path", "centreon_nagvis_uri")';
@@ -40,7 +39,8 @@ function debug($msg) {
   fclose($fh);
 }
 
-$userCentreon = $roUser;
+// FIXME Should be a pref
+$single_nagvis_user = false;
 
 /* Fix bad usage */
 $_SERVER['SCRIPT_FILENAME'] = $nagvis_path . 'frontend/nagvis-js/index.php';
@@ -54,26 +54,69 @@ require_once $nagvis_path . 'server/core/classes/CoreExceptions.php';
 require_once $nagvis_path . 'server/core/functions/nagvisErrorHandler.php';
 require_once $nagvis_path . 'server/core/functions/core.php';
 
+// FIXME Various requires should probably be initialized on top
+require_once $centreon_path . "/www/class/centreonLang.class.php";
+$centreonLang = new CentreonLang($centreon_path, $centreon);
+$centreonLang->bindLang();
+$centreonLang->bindLang('messages');
+
 /*
  * Init Global
  */
 $core = GlobalCore::getInstance();
-$listMap = $core->getAvailableMaps();
+
+if ($single_nagvis_user) {
+    // FIXME Should be a pref
+    $userCentreon = 'centreon_nagvis2';
+} else {
+    $userCentreon = $centreon->user->alias;
+}
+
+$error = '';
+$listMap = array();
 
 /* 
  * Create nagvis session 
  */
 $AUTH = new CoreAuthHandler();
 if ($AUTH->checkUserExists($userCentreon)) {
-  $credential = array('user' => $userCentreon);
-  $AUTH->setTrustUsername(true);
-  $AUTH->setLogoutPossible(true);
-  $AUTH->passCredentials($credential);
-  $_SESSION['logonModule'] = cfg('global', 'logonmodule');
-  $_SESSION['authModule'] = $AUTH->getModule();
-  $_SESSION['authCredentials'] = $credential;
-  $_SESSION['authTrusted'] = $AUTH->authedTrusted();
-  $_SESSION['authLogoutPossible'] = $AUTH->logoutSupported();
+    $credential = array('user' => $userCentreon);
+    $AUTH->setTrustUsername(true);
+    $AUTH->setLogoutPossible(true);
+    $AUTH->passCredentials($credential);
+    $_SESSION['logonModule'] = cfg('global', 'logonmodule');
+    $_SESSION['authModule'] = $AUTH->getModule();
+    $_SESSION['authCredentials'] = $credential;
+    $_SESSION['authTrusted'] = $AUTH->authedTrusted();
+    $_SESSION['authLogoutPossible'] = $AUTH->logoutSupported();
+
+    if ($single_nagvis_user) {
+        $listMap = $core->getAvailableMaps();
+    } else {
+        if ($AUTH->isAuthenticated()) {
+            $AUTHORISATION = new CoreAuthorisationHandler();
+            $AUTHORISATION->parsePermissions();
+
+            $core->setAA($AUTH, $AUTHORISATION);
+            $list = Array();
+            $maps = $core->getAvailableMaps();
+            foreach ($maps AS $key => $val) {
+                if ($core->getAuthorization()->isPermitted('Map', 'view', $val)) {
+                    $list[$key] = $val;
+                }
+            }
+            if (empty($list)) {
+                $error =  _("No map available");
+            } else {
+                $listMap = $list;
+            }
+        } else {
+            $AUTHORISATION = null;
+            $error = _("NagVis authentication failed");
+        }
+    }
+} else {
+    $error = _("No such user in NagVis: ") . $userCentreon;
 }
 
 chdir($tmpdir);
@@ -85,6 +128,7 @@ sort($listMap);
  * over first versions of this module. Some client are using URL with station, we cannot break compatibility
  */
 $currentMap = '';
+$mapurl = '';
 if (isset($_GET["map"])) {
   $currentMap = $_GET["map"];
   $mapurl = $nagvis_uri . '?mod=Map&context_menu=0&hover_menu=1&header_menu=0&show=' . $_GET["map"];
@@ -105,6 +149,7 @@ if (isset($_GET["map"])) {
 $tpl = new Smarty();
 $tpl = initSmartyTpl($path, $tpl);
 
+$tpl->assign('error', $error);
 $tpl->assign('listmap', $listMap);
 $tpl->assign('nagvis_uri', $nagvis_uri);
 $tpl->assign('mapurl', $mapurl);
