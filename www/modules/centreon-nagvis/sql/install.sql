8ec5f05574378fb708efb63073662b1df56d4a93
Pierre Villard <pierre.villard.fr@gmail.com>
2016-01-28 14:18:02 +0100

    Updated install/uninstall SQL for Centreon 2.7.1
    Updated install/uninstall SQL for Centreon 2.7.1

--- www/modules/centreon-nagvis/sql/install.sql
+++ www/modules/centreon-nagvis/sql/install.sql
@@ -1,13 +1,13 @@
--- Topology
-INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`) VALUES ('', 'Nagvis', NULL, 4, 403, 20, 1, './modules/centreon-nagvis/index.php', NULL, '0', '1', '1');
+-- Topology -- Main page
+INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`, `readonly`) VALUES ('', 'Nagvis', NULL, NULL, 4, 30, 1, './modules/centreon-nagvis/index.php', NULL, '0', '0', '1', NULL, NULL, NULL, '1');
+-- Topology -- Subpage
+INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`, `readonly`) VALUES ('', 'Nagvis', NULL, 4, 403, 1, 1, './modules/centreon-nagvis/index.php', NULL, '0', '0', '1', NULL, NULL, NULL, '1');
 -- Admin page
 INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`) VALUES ('', 'Nagvis', NULL, 5, 513, 11, 1, './modules/centreon-nagvis/nagvis-config.php', NULL, '0', '1', '1');
 INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`) VALUES ('', 'Nagvis Configuration', NULL, 513, 51301, 20, 1, './modules/centreon-nagvis/nagvis-config.php', NULL, '0', '1', '1');
 
-
 -- Insert options
 INSERT INTO `options` (`key`,`value`) VALUES ('centreon_nagvis_uri','/nagvis/frontend/nagvis-js/index.php');
 INSERT INTO `options` (`key`,`value`) VALUES ('centreon_nagvis_path','/usr/local/nagvis/share/');
-
 INSERT INTO `options` (`key`, `value`) VALUES ('centreon_nagvis_auth', 'single');
 INSERT INTO `options` (`key`, `value`) VALUES ('centreon_nagvis_single_user', 'centreon_nagvis');
--- www/modules/centreon-nagvis/sql/uninstall.sql
+++ www/modules/centreon-nagvis/sql/uninstall.sql
@@ -2,6 +2,9 @@
 DELETE FROM `topology` WHERE `topology_page` = '51301';
 DELETE FROM `topology` WHERE `topology_page` = '513';
 DELETE FROM `topology` WHERE `topology_page` = '403';
+DELETE FROM `topology` WHERE `topology_page` = '4';
 
 DELETE FROM `options` WHERE `key` = 'centreon_nagvis_uri';
 DELETE FROM `options` WHERE `key` = 'centreon_nagvis_path';
+DELETE FROM `options` WHERE `key` = 'centreon_nagvis_auth';
+DELETE FROM `options` WHERE `key` = 'centreon_nagvis_single_user';
